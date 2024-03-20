0d00dedf1cea04ae964efaf123975de5efa3a148
VinzelOps <88572751+VinzelOps@users.noreply.github.com>
2024-03-20 12:25:59 +0700

    adds

---
+++ Assign-2/database/config.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"go-rest-api/models"
+	"
+gorm.io
+/driver/postgres"
+	"
+gorm.io
+/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "user=postgres password=postgresGO dbname=go_rest_api port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	DB = db
+
+	db.AutoMigrate(&models.Paket{})
+	db.AutoMigrate(&models.Order{})
+}
